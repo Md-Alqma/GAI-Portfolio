c6e86b48342d04ff9ea3e4b259ff81af5f860aa4
Md-Alqma <mdalqma7@gmail.com>
2023-08-16 02:31:58 +0530

    completed contact me section

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // ... Previous code ...
+
+  // Form validation
+  const contactForm = document.querySelector("#contactForm");
+
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Validate form fields (add more validation as needed)
+    const name = contactForm.querySelector("#name").value.trim();
+    const email = contactForm.querySelector("#email").value.trim();
+    const message = contactForm.querySelector("#message").value.trim();
+
+    // if (!name || !email || !message) {
+    //   alert("Please fill in all fields.");
+    //   return;
+    // } else {
+    // Send the form (you can implement sending logic here)
+    alert("Form submitted successfully!");
+    contactForm.reset();
+  });
+});
