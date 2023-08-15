46b0f779d2f83816b0c3d4ebe58604c5c171f9cf
Md-Alqma <mdalqma7@gmail.com>
2023-08-16 02:20:27 +0530

    skills and portfolio section

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("show");
     menuToggle.classList.toggle("active");
   });
+
+  // Close the navbar menu when clicking on a link
+  const navItems = document.querySelectorAll(".nav-links a");
+  navItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      navLinks.classList.remove("show");
+      menuToggle.classList.remove("active");
+    });
+  });
 });
-  
